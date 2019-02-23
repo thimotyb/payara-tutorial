5730fd6a2b401341fc8a383309ce8cb88c7aa846
Thimoty Barbieri <thimoty@thimoty.it>
2019-02-23 16:59:43 +0100

    Code and dependencies

---
+++ src/main/java/it/corso/KafkaMDB.java
@@ -0,0 +1,28 @@
+package it.corso;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import fish.payara.cloud.connectors.kafka.api.KafkaListener;
+import fish.payara.cloud.connectors.kafka.api.OnRecord;
+
+@MessageDriven(activationConfig = {
+	    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "testClient"),
+	    @ActivationConfigProperty(propertyName = "groupIdConfig", propertyValue = "test-consumer-group"),
+	    @ActivationConfigProperty(propertyName = "topics", propertyValue = "testing"),
+	    @ActivationConfigProperty(propertyName = "bootstrapServersConfig", propertyValue = "192.168.29.139:9092"),   
+	    @ActivationConfigProperty(propertyName = "autoCommitInterval", propertyValue = "100"),   
+	    @ActivationConfigProperty(propertyName = "retryBackoff", propertyValue = "1000"),   
+	    @ActivationConfigProperty(propertyName = "keyDeserializer", propertyValue = "org.apache.kafka.common.serialization.StringDeserializer"),   
+	    @ActivationConfigProperty(propertyName = "valueDeserializer", propertyValue = "org.apache.kafka.common.serialization.StringDeserializer"),   
+	    @ActivationConfigProperty(propertyName = "pollInterval", propertyValue = "1000"),   
+	})
+	public class KafkaMDB implements KafkaListener {
+	     
+	    @OnRecord( topics={"testing"})
+	    public void getMessageTest(ConsumerRecord record) {
+	        System.out.println("Got record on topic testing " + record);
+	    }
+	}
